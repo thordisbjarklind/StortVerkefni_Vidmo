e400b1d4907d1f20b5f5a570c2fed32066da588e
Elixigol <logicool112@gmail.com>
2026-04-10 15:49:31 +0000

    Bæta við LeikStillingu (interface milli fxml skráa) og StartController fyrir upphafsskjá

--- src/main/java/is/vinnsla/LeikStilling.java
+++ src/main/java/is/vinnsla/LeikStilling.java
@@ -1,4 +1,13 @@
 package is.vinnsla;
 
 public class LeikStilling {
+    private final Leikmadur[] leikmenn;
+
+    public LeikStilling(Leikmadur[] leikmenn) {
+        this.leikmenn = leikmenn;
+    }
+
+    public Leikmadur[] getLeikmenn() {
+        return leikmenn;
+    }
 }
